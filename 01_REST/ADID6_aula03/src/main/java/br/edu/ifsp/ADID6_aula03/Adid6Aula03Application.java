8dc58c0697ced930f53f1193cdc9b43d89f495d1
lucaspassos858 <lucaspassos858@gmail.com>
2021-04-12 18:07:34 -0300

    Sprint initializr

---
+++ 01_REST/ADID6_aula03/src/main/java/br/edu/ifsp/ADID6_aula03/Adid6Aula03Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.ADID6_aula03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Adid6Aula03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Adid6Aula03Application.class, args);
+	}
+
+}
---
+++ 01_REST/ADID6_aula03/src/test/java/br/edu/ifsp/ADID6_aula03/Adid6Aula03ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.ADID6_aula03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Adid6Aula03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
